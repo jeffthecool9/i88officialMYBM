4c7c4534d158cc3ffc8be8629f7cccab5c2b01fc
jeffthecool9 <107611378+jeffthecool9@users.noreply.github.com>
2026-06-15 13:19:00 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,12 @@ export default function App() {
       <EventCarousel />
       <HowToJoin />
       <StickyCTA />
+
+        {/* 18+ notice */}
+      <p className="text-center font-body text-ice/20 text-xs px-6 py-6 leading-relaxed pb-24">
+       Anda akan dialihkan secara automatik ke laman web rasmi. Sila jangan risau sekiranya nama jenama kelihatan berbeza, kerana perkara ini adalah disebabkan oleh keperluan polisi.
+
+      </p>
     </div>
   )
 }
